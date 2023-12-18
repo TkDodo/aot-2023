b4de61e1e5c509d0d2c21d2c3ab71a0ce0ce8630
Dominik Dorfmeister <office@dorfmeister.cc>
2023-12-18 07:55:44 +0100

    Update 18.ts

--- 18.ts
+++ 18.ts
@@ -4,3 +4,15 @@ type Count<TItems extends Array<any>, TSearch extends string, TAcc extends Array
       ? Count<Tail, TSearch, [...TAcc, TSearch]>
       : Count<Tail, TSearch, TAcc>
     : TAcc['length']
+
+// alternative without Accumulator
+
+type Filter<TItems extends Array<any>, TSearch extends string> =
+  TItems extends [infer Head, ...infer Tail]
+    ? Head extends TSearch
+      ? [Head, ...Filter<Tail, TSearch>]
+      : Filter<Tail, TSearch>
+    : []
+
+type Count<TItems extends Array<any>, TSearch extends string> =
+  Filter<TItems, TSearch>['length']
